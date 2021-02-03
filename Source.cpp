313a1f2df367d0f983ddb87664b7116cad7cb23e
Александр Шварц <Aleksandr5xz@gmail.com>
2021-02-03 23:03:15 +0300

    aaaa

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-int add(int a, int b) {
-	return a + b;
- }
+
 int main()
 {
 	std::cout << "Hello World" << std::endl;
-	//https://github.com/Nutsheil/testgit
+
 
 	return 0;
 }
